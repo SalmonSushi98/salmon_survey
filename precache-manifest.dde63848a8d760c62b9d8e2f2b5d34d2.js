54d9eda3b0b6b6c99211ca6cd70838ab60c46ff2
SalmonSushi <stephen_shin@naver.com>
2020-08-22 13:51:42 +0900

    Updates

---
+++ precache-manifest.dde63848a8d760c62b9d8e2f2b5d34d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7248311a98f7ff7a1f4ef2a1f8452a0",
+    "url": "/salmon_survey/index.html"
+  },
+  {
+    "revision": "92df909728e546200fc6",
+    "url": "/salmon_survey/static/css/main.2bafe603.chunk.css"
+  },
+  {
+    "revision": "f598df1801fd03900374",
+    "url": "/salmon_survey/static/js/2.1b276a2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/salmon_survey/static/js/2.1b276a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92df909728e546200fc6",
+    "url": "/salmon_survey/static/js/main.f990ef52.chunk.js"
+  },
+  {
+    "revision": "e972865cfb1a24cef0f7",
+    "url": "/salmon_survey/static/js/runtime-main.d6aa76e6.js"
+  }
+]);
